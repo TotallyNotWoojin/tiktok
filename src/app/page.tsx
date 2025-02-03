f27b4a3782de69d25d717aad0db68c6b39ff2032
Jad Alriyabi <71855107+Jadalriyabi@users.noreply.github.com>
2025-02-02 23:16:05 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Link from 'next/link'
+import classNames from 'classnames'
+import classNames from 'classnames'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold mb-8">TikTok Clone</h1>
-      <div className="space-x-4">
-        <Link href="/login" className="px-4 py-2 bg-black text-white rounded">Login</Link>
-        <Link href="/register" className="px-4 py-2 border border-black rounded">Register</Link>
+        <Link href="/login" className={classNames('px-4', 'py-2', 'bg-black', 'text-white', 'rounded')}>Login</Link>
+        <Link href="/register" className={classNames('px-4', 'py-2', 'border', 'border-black', 'rounded')}>Register</Link>
+        <Link href="/register" className={classNames('px-4', 'py-2', 'border', 'border-black', 'rounded')}>Register</Link>
       </div>
     </main>
   )
